8dc7fedfe6253d079fdffab796847d829a159504
Andy Allsopp <arallsopp@gmail.com>
2026-01-21 22:41:53 +0000

    got a question to the screen.

---
+++ src/components/Question.jsx
@@ -0,0 +1,11 @@
+function Question({ questionData }) {
+    return (
+      <div>
+        <h2>{questionData.question}</h2>
+        {/* todo  add the answers here next */}
+      </div>
+    )
+  }
+  
+  export default Question
+ 
