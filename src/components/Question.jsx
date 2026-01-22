f747e6c2f1b59baf8b48345ca6e64e68f6eff8e5
Andy Allsopp <arallsopp@gmail.com>
2026-01-22 01:19:05 +0000

    shuffle answers

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
+
+
+// Fisher–Yates shuffle
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
 
 function Question({ questionData, onAnswer }) {
+    
+    //store original position.
+    const withIndex = questionData.answers.map((text, index) => ({
+        text,
+        originalIndex: index
+    }));
+
+    shuffle(withIndex);
+
     return (
       <div>
         <small>Question: {questionData.id}</small>
         <h2>{questionData.question}</h2>
-        {questionData.answers.map((answer,index) => (
+        {withIndex.map((answer) => (
             <button 
-            key={index} 
+            key={answer.originalIndex} 
             className="answer"
             onClick={() => {
-                console.log('component saw answer click',index);
-                onAnswer(index)
-            }}>{answer}</button>
+                console.log('component saw answer click',answer.originalIndex);
+                onAnswer(answer.originalIndex)
+            }}>{answer.text}</button>
         ))}   
       </div>
     )
