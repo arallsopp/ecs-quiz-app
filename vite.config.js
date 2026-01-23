19d7278ddde45b527484f1b5b3bf8af079a63193
Andy Allsopp <arallsopp@gmail.com>
2026-01-23 13:17:23 +0000

    fixing github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ecs-quiz-app/' // <-- this is the folder within github pages
 })
