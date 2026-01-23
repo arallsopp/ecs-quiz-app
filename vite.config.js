4db04cb9f88ec59c55dff51205037bc3d8e1f478
Andy Allsopp <arallsopp@gmail.com>
2026-01-23 20:06:36 +0000

    added icons, vite-plugin-pwa, manifest and workbox for all public assets.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,79 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+      // add this to cache all the imports
+      workbox: {
+          globPatterns: ["**/*"],
+      },
+      // add this to cache all the
+      // static assets in the public folder
+      includeAssets: [
+          "**/*",
+      ],
+      manifest: {
+          "name": "ECS Health and Safety Quiz",
+          "short_name": "ECS HS Quiz",
+          "icons": [
+              {
+                  "src": "/icon-48x48.png",
+                  "sizes": "48x48",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/icon-72x72.png",
+                  "sizes": "72x72",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/icon-96x96.png",
+                  "sizes": "96x96",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/icon-128x128.png",
+                  "sizes": "128x128",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/icon-144x144.png",
+                  "sizes": "144x144",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/icon-152x152.png",
+                  "sizes": "152x152",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/icon-192x192.png",
+                  "sizes": "192x192",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/icon-256x256.png",
+                  "sizes": "256x256",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/icon-384x384.png",
+                  "sizes": "384x384",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/icon-512x512.png",
+                  "sizes": "512x512",
+                  "type": "image/png"
+              }
+          ],
+          "start_url": "/",
+          "display": "standalone",
+          "background_color": "#ffffff",
+          "theme_color": "#000000"
+      }}
+  )],
   base: '/ecs-quiz-app/' // <-- this is the folder within github pages
 })
